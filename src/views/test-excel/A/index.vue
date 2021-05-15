987d850de5f0474bdaa166449a98243d6a80aeb2
chenkun <1119514898@qq.com>
2021-05-15 17:24:24 +0800

    test: 测试webhooks-6

--- src/views/test-excel/A/index.vue
+++ src/views/test-excel/A/index.vue
@@ -255,6 +255,7 @@ export default {
 
       // 将数每行格式化成想要的数据值
       const formatData = formatJson(cols, data)
+      console.log(formatData)
 
       // 合计计算
       const totalObj = calcArrayTotal(
